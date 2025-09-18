6556e15d33fe7fb5f6986c71d7b617199510e7b2
Manjoor Shaik <manjoorsk.fullstackdev@gmail.com>
2025-09-18 09:29:25 +0530

    test4

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
 const JWT_Key = "manjoor5678";
 
-//Auth Middleware:
+//Auth Middleware
 export const authenticateToken = (
   req: Request,
   res: Response,
