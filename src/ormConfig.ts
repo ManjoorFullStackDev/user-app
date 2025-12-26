8bed27cbf80c8983ce755ef9fbd7b2950f3e8b03
Manjoor Shaik <manjoorsk.fullstackdev@gmail.com>
2025-12-26 21:11:56 +0530

    updated db env

--- src/ormConfig.ts
+++ src/ormConfig.ts
@@ -6,12 +6,12 @@
 
   export const AppDataSource = new DataSource({
     type: "mssql",
-    host: "localhost",
+    host: process.env.DB_HOST,
     port: Number(process.env.DB_PORT),
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    synchronize: true,
+    synchronize: true,  
     logging: true,
     entities: [User, Task],
     options: {
